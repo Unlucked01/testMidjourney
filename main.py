9338874210f8f3ba0eb5606ce0f54bf62b1860da
Unlucked01 <113236909+Unlucked01@users.noreply.github.com>
2023-11-07 19:33:18 +0300

    applied Dalle -3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot
 from aiogram import Dispatcher
 from aiogram import types
+from openai import OpenAI
 
 import os
 import replicate
@@ -15,26 +16,20 @@ dotenv.load_dotenv()
 
 os.environ["REPLICATE_API_TOKEN"] = os.getenv('REPLICATE_KEY')
 
+client = OpenAI()
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher()
 
 
-async def get_image(prompt: str, width=768, height=768, negative="None"):
-    loop = asyncio.get_event_loop()
-
-    def generate_image():
-        return replicate.run(
-            "prompthero/openjourney:ad59ca21177f9e217b9075e7300cf6e14f7e5b4505b87b9689dbd866e9768969",
-            input={"prompt": "mdjrny-v4 " + prompt, "negative_prompt": negative,
-                   "width": width, "height": height}
-        )
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(generate_image)
-        output = await loop.run_in_executor(None, future.result)
-
-    for image in output:
-        return image
+async def get_image(prompt):
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    return response.data[0].url
 
 
 @dp.message()
@@ -43,11 +38,14 @@ async def get_message(message: types.Message):
         chat_id=message.chat.id,
         text="Генерация изображения...",
     )
-    out = await get_image(message.text)
-    if out:
-        await message.reply_photo(
-            photo=out,
-        )
+    if message.text:
+        out = await get_image(message.text)
+        if out:
+            await message.reply_photo(
+                photo=out,
+            )
+        else:
+            await message.reply("Не удалось сгенерировать картинку")
     else:
         await message.reply("Не удалось обработать запрос")
 
