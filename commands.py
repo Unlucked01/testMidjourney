f1c97abfb1c4384a4a329e2ed5b0cb52440224dd
Unlucked01 <113236909+Unlucked01@users.noreply.github.com>
2023-11-24 15:57:51 +0300

    new main

---
+++ commands.py
@@ -0,0 +1,12 @@
+# Commands
+start_command = 'start'
+menu_command = 'menu'
+
+generate = 'generate_image'
+redact = 'redact_image'
+
+# Messages
+start_message = 'Bot start message'
+menu_message = 'Menu message'
+
+back_button = 'Вернуться в меню'
--- new_main.py
+++ new_main.py
@@ -5,11 +5,14 @@ from aiogram import Bot, F
 from aiogram import Dispatcher
 from aiogram import types
 from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from openai import AsyncOpenAI, APIError
 from aiogram import Router
 
 import os
 import dotenv
+
+import commands
 
 dotenv.load_dotenv()
 
@@ -17,6 +20,39 @@ client = AsyncOpenAI()
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 dp = Dispatcher()
+
+
+menu_markup = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text=commands.generate), KeyboardButton(text=commands.redact)],
+        [KeyboardButton(text=commands.menu_command)],
+    ],
+    resize_keyboard=True
+)
+
+
+@dp.message(Command(commands.start_command))
+async def welcome_command(message: types.Message):
+    await message.answer(commands.start_message)
+    await menu_command(message)
+
+
+@dp.message(Command(commands.menu_command))
+@dp.message(F.text == commands.back_button)
+async def menu_command(message: types.Message):
+    await message.answer(commands.menu_message, reply_markup=menu_markup)
+
+
+@dp.message(Command(commands.menu_command))
+@dp.message(F.text == commands.generate)
+async def generate(message: types.Message):
+    await message.answer(commands.generate, reply_markup=menu_markup)
+
+
+@dp.message(Command(commands.menu_command))
+@dp.message(F.text == commands.redact)
+async def generate(message: types.Message):
+    await message.answer(commands.redact, reply_markup=menu_markup)
 
 
 async def get_image(prompt):
@@ -30,7 +66,7 @@ async def get_image(prompt):
     return response.data[0].url
 
 
-@dp.message()
+@dp.message(Command(commands.generate))
 async def get_message(message: types.Message):
     await bot.send_message(
         chat_id=message.chat.id,
@@ -47,6 +83,11 @@ async def get_message(message: types.Message):
             await message.reply("Не удалось сгенерировать картинку")
     else:
         await message.reply("Не удалось обработать запрос")
+
+
+@dp.message(Command(commands.generate))
+async def redact_image(message: types.Message):
+    await message.answer("handle redact command")
 
 
 async def main():
